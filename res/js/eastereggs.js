e7475a621fd94f6f6bf6870c033a985b8937847f
Austin Liu <atrix315@gmail.com>
2016-12-19 16:43:54 -0500

    um yeah easter eggs?

---
+++ res/js/eastereggs.js
@@ -0,0 +1,11 @@
+cheet('p a x o s', function () {
+  alert('god mode enabled');
+});
+
+cheet('$ h a t t', function () {
+  alert('OMG $HATT5EVER!');
+});
+
+cheet('m u a h', function () {
+  alert('<3 么么哒');
+});

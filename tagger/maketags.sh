22921a2a34aa9ae01ffcbcf582d830e88e9455b7
Jared <jmw535@cornell.edu>
2016-12-19 23:19:29 -0800

    change maketags to do all files in directory

--- tagger/maketags.sh
+++ tagger/maketags.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 rm dictionaries/*
-./tagger.py training/demo.txt
-./tagger.py training/washu_demo1.txt
-./tagger.py training/washu_demo2.txt
-./tagger.py training/washu_demo3.txt
-./tagger.py training/washu_demo4.txt
-./tagger.py training/angularjs.txt
-./tagger.py training/reactjs.txt
+for file in training/*
+do
+    ./tagger.py $file
+done
+#./tagger.py training/demo.txt
+#./tagger.py training/washu_demo1.txt
+#./tagger.py training/washu_demo2.txt
+#./tagger.py training/washu_demo3.txt
+#./tagger.py training/washu_demo4.txt
+#./tagger.py training/angularjs.txt
+#./tagger.py training/reactjs.txt
